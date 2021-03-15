f3fc0c1e3741328ffb0573740ae3e868b7f59263
AzureAD\MarioGomesdaSilva <mario.gomes1@ibm.com>
2021-03-15 14:24:07 -0300

    Create IMovieRepository.cs interface

---
+++ MyMoviesV2/Repositories/IMovieRepository.cs
@@ -0,0 +1,17 @@
+﻿using MyMoviesV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMoviesV2.Repositories
+{
+    interface IMovieRepository
+    {
+        Task<IEnumerable<Movie>> Get();
+        Task<Movie> Get(int Id);
+        Task<Movie> Update(Movie movie);
+        Task Create(Movie movie);
+        Task Delete(int Id);
+    }
+}
--- MyMoviesV2/Startup.cs
+++ MyMoviesV2/Startup.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using MyMoviesV2.Models;
 
 namespace MyMoviesV2
 {
